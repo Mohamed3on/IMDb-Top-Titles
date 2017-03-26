d17521e12442b37ae6294b5d75e23ee829d3de2c
Mohamed Oun <mohamed3on@gmail.com>
2017-03-26 22:16:55 +0200

    convert to html unicode

--- functions.py
+++ functions.py
@@ -1,19 +1,19 @@
+import cgi
 import json
-from selenium import webdriver
-from bs4 import BeautifulSoup
 import urllib.request
+from pathlib import Path
+
+import bs4
+from selenium import webdriver
 
 
-def getScore(id):
+def getEpscore(id):
     url = 'http://www.imdb.com/title/' + id + '/ratings'
-    with urllib.request.urlopen(url) as url:
-        r = url.read()
-    soup = BeautifulSoup(r, "lxml")
+    soup = getSoup(url)
     ratings = []
     previous = 1
     for i in soup.find_all("a", class_="main"):
         name = i.string
-        print(name)
 
     for link in soup.find_all('td'):
         if link.get('nowrap') == "1":
@@ -22,23 +22,54 @@ def getScore(id):
             previous = link.string
         if len(ratings) == 10: break
     score = ratings[0] + ratings[1] - ratings[-1] - ratings[-2]
-    print("score: ", score)
+    return name, score, sum(ratings)
+
+
+def getScore(id):
+    url = 'http://www.imdb.com/title/' + id + '/ratings'
+    soup = getSoup(url)
+    ratings = []
+    previous = 1
+    for i in soup.find_all("a", class_="main"):
+        name = i.string
+
+    for link in soup.find_all('td'):
+        if link.get('nowrap') == "1":
+            ratings.append(int(previous))
+        else:
+            previous = link.string
+        if len(ratings) == 10: break
+    score = ratings[0] + ratings[1] - ratings[-1] - ratings[-2]
     return name, score
+
+
+def getSoup(url):
+    with urllib.request.urlopen(url) as url:
+        r = url.read()
+    return bs4.BeautifulSoup(r, "lxml")
 
 
 # if found item with class next-page go to that item's href and append the scores, else return the scores
 def getMovies(scores, url, minScore):
-    with urllib.request.urlopen(url) as url:
-        r = url.read()
-    soup = BeautifulSoup(r, "lxml")
-    for ana in soup.find_all("span", class_="lister-item-header"):
-        url = ana.a["href"]
+    soup = getSoup(url)
+    bypassed = 0
+    for movie in soup.find_all("span", class_="lister-item-header"):
+        if bypassed > 30:
+            savescores(scores, 'scores')
+            return scores
+        for title in movie.find_all("a"):
+            url = title["href"]
+            moviename = title.text
         id = url.split('/')[2]
-        moviename = ana.a.text
         if moviename not in scores:
             name, score = getScore(id)
             if score > minScore:
-                scores[name] = score
+                bypassed = 0
+                scores[name] = score, id
+                print(name)
+                print(scores[name][0])
+            else:
+                bypassed += 1
         else:
             continue
     savescores(scores, 'scores')
@@ -54,17 +85,40 @@ def getMovies(scores, url, minScore):
         return getMovies(scores, nexturl, minScore)
 
 
-def savescores(scores, name):
-    name = name + '.json'
-    with open(name, 'w') as fp:
-        json.dump(scores, fp)
+def getEpisodes(url):
+    episodes = {}
+    notselected = 0
+    soup = getSoup(url)
+    table = soup.find("table")
+    position = 1
+    for ep in table.find_all("tr"):
+        if position == 1:
+            position += 1
+            continue
+        if notselected >= 10:
+            break
+
+        href = ep.find("a")["href"]
+        id = href.split('/')[2]
+        episode = ep.find("td").text
+        episode = episode.replace(u'\xa0', u'')
+        name, score, sum = getEpscore(id)
+        if score / sum > 0.5:
+            notselected = 0
+            print(episode, name, score)
+            episodes[str(episode)] = score, name
+
+        else:
+            notselected += 1
+            continue
+    return episodes
 
 
 def getBookScore(url, driver):
     driver.get(url)
     driver.find_element_by_id("rating_details").click()
     html = driver.page_source
-    soup = BeautifulSoup(html, "lxml")
+    soup = bs4.BeautifulSoup(html, "lxml")
     scores = []
     for td in soup.find_all("td", width="90"):
         s = td.text
@@ -72,25 +126,49 @@ def getBookScore(url, driver):
     return scores[0] - scores[-1]
 
 
-def getBooks(books, url):
+def getBooks(url):
+    books = {}
     chromeOptions = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images": 2}
     chromeOptions.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome("C:/Users/Mohamed/chromedriver.exe", chrome_options=chromeOptions)
-    with urllib.request.urlopen(url) as url:
-        r = url.read()
-    soup = BeautifulSoup(r, "lxml")
-    count = 1
+    soup = getSoup(url)
+    bypassed = 1
     for book in soup.find_all("a", class_="bookTitle"):
         title = book.text
-        if title not in books:
-            href = "https://www.goodreads.com" + book["href"]
-            score = getBookScore(href, driver)
-            print(count, ':', title)
-            print(score)
-            books[title] = score
-            count += 1
-        else:
-            continue
+
+        href = "https://www.goodreads.com" + book["href"]
+        score = getBookScore(href, driver)
+        print(bypassed, ':', title)
+        print(score)
+        books[title] = score
+        bypassed += 1
+
     driver.close()
     return books
+
+
+def savescores(scores, name):
+    name += '.json'
+    with open(name, 'w') as fp:
+        json.dump(scores, fp)
+
+
+def loadfile(name):
+    my_file = Path(name + ".json")
+    if my_file.is_file():
+        json1_file = open(name + '.json')
+        json1_str = json1_file.read()
+        file = json.loads(json1_str)
+    else:
+        file = {}
+    return file
+
+
+def unicodeToHTMLEntities(text):
+    """Converts unicode to HTML entities.  For example '&' becomes '&amp;'."""
+    text = cgi.escape(text).encode('ascii', 'xmlcharrefreplace')
+    if str(text).find("&") == -1:
+        return str(text)[1:]
+    else:
+        return str(text).split('b')[1].split("\'")[1]
--- movies.py
+++ movies.py
@@ -1,20 +1,18 @@
-import json
-from pathlib import Path
-
-import functions as imdb
 import operator
 
+import functions
+
 minScore = 10000
-my_file = Path("sorted.json")
-if my_file.is_file():
-    json1_file = open('sorted.json')
-    json1_str = json1_file.read()
-    scores = json.loads(json1_str)
-else:
-    scores = {}
-data = imdb.getMovies(scores,
-                      'http://www.imdb.com/search/title?count=250&num_votes=30000,&sort=num_votes,desc&view=simple&page=15&ref_=adv_prv',
-                      minScore)
-sorteddata = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
-imdb.savescores(data, 'scores')
-imdb.savescores(sorteddata, 'sortedtitles')
+# for testing purposes
+scores = functions.loadfile('scores')
+# scores = imdb.getMovies(scores,
+#                       'http://www.imdb.com/search/title?num_votes=1000,&sort=user_rating,desc&title_type=tv_episode&view=simple',
+#                       minScore)
+
+sortedscores = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
+functions.savescores(sortedscores, 'sortedtitles')
+newsorted = functions.loadfile('sortedtitles')
+for score in newsorted:
+    score[0] = functions.unicodeToHTMLEntities(score[0])
+
+functions.savescores(newsorted, 'sortedtitles')
