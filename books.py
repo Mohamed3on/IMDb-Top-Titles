ff54ff0c920b7e0c6934e0d9f4cb7894e5d5fe3f
Mohamed Oun <mohamed3on@gmail.com>
2017-03-30 14:55:53 +0200

    Clean things up

--- books.py
+++ books.py
@@ -11,31 +11,17 @@ def unicodeToHTMLEntities(text):
     text = cgi.escape(text).encode('ascii', 'xmlcharrefreplace')
     return str(text).split('b')[1].split("\'")[1]
 
-
-def programmingbooks():
-    my_file = Path("programmingbooks.json")
-    if my_file.is_file():
-        json1_file = open('programmingbooks.json')
-        json1_str = json1_file.read()
-        books = json.loads(json1_str)
-    else:
-        books = {}
-    data = functions.getBooks(books, "https://www.goodreads.com/shelf/show/programming")
+def savebooks(url,name):
+    data = functions.getBooks(url)
     sorteddata = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
-    functions.savescores(sorteddata, 'programmingbooks')
+    functions.savescores(sorteddata, name)
+def programmingbooks():
+    savebooks("https://www.goodreads.com/shelf/show/programming",'programmingbooks')
 
 
 def generalbooks():
-    my_file = Path("generalbooks.json")
-    if my_file.is_file():
-        json1_file = open('generalbooks.json')
-        json1_str = json1_file.read()
-        books = json.loads(json1_str)
-    else:
-        books = {}
-    data = functions.getBooks(books, "https://www.goodreads.com/book/most_read?category=all&country=all&duration=y")
-    sorteddata = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
-    functions.savescores(sorteddata, 'generalbooks')
+    savebooks("https://www.goodreads.com/book/most_read?category=all&country=all&duration=y",'generalbooks')
 
-
-generalbooks()
+def businessbooks():
+    savebooks("https://www.goodreads.com/shelf/show/business",'businessbooks')
+businessbooks()
