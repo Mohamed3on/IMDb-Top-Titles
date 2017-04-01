0ea6460a3ef9ea15c9ec775450ef8d4c156ba13c
Mohamed Oun <mohamed3on@gmail.com>
2017-04-01 13:19:49 +0200

    organizing

--- movies.py
+++ movies.py
@@ -3,14 +3,17 @@ import operator
 import functions
 
 minScore = 10000
-# for testing purposes
-# scores = functions.loadfile('scores')
-scores = {}
+minVotes = 25000
+# to resume execution if the script fails while running
+scores = functions.loadfile('scores')
 scores = functions.getMovies(
-    scores, 'http://www.imdb.com/search/title?count=250&num_votes=30000,&sort=num_votes,desc&view=simple', minScore)
+    scores,
+    'http://www.imdb.com/search/title?count=250&num_votes=' + str(minVotes) + ',&sort=num_votes,desc&view=simple',
+    minScore)
 
 sortedscores = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
 functions.savescores(sortedscores, 'sortedtitles')
+## This is a hack to get the desired format to operate on
 newsorted = functions.loadfile('sortedtitles')
 for score in newsorted:
     score[0] = functions.unicodeToHTMLEntities(score[0])
