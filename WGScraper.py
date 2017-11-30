97b5b77b06437b5e1bdd4b652006675b2646b4d9
Mohamed Oun <mohamed3on@gmail.com>
2017-11-30 16:13:27 +0100

    Update wg-gesucht scraper

--- WGScraper.py
+++ WGScraper.py
@@ -1,54 +1,44 @@
-import html
 import time
+from commonfunctions import getSoup, savescoresUnicode
 
-import bs4
-from numpy import save
-
-from commonfunctions import getSoup, loadfile, savescores
-
-offers = loadfile('offers')
-
-# def setup_driver():
-#     options = webdriver.ChromeOptions()
-#     prefs = {"profile.managed_default_content_settings.images": 2}
-#     options.add_experimental_option("prefs", prefs)
-#     thedriver = webdriver.Chrome(chrome_options=options)
-#     return thedriver
+newoffers = {}
 
 
 def find_new_offers():
-    soup = getSoup('http://www.wg-gesucht.de/en/wg-zimmer-in-Berlin.8.0.1.0.html?offer_filter=1&city_id=8&category=0&rent_type=0&rMax=700&dFr=1505858400&wgSea=2&wgSmo=2&wgAge=22&wgMnF=1&fur=1')
+    soup = getSoup('http://www.wg-gesucht.de/en/wg-zimmer-in-Berlin.8.0.1.0.html?offer_filter=1&noDeact=1&city_id=8&category=0&rent_type=0&rMax=800&dFr=1512082800&dTo=1513724400&radLat=52.5306438&radLng=13.38306829999999&radAdd=Mitte%2C+Berlin%2C+Germany&radDis=8000&wgSea=2&wgAge=22&wgMnF=1&fur=1')
     counter = 0
-    for title in soup.find_all("h3", class_="headline headline-list-view noprint"):
+    for title in soup.find_all("h3", class_="headline headline-list-view noprint truncate_title"):
         counter = counter + 1
         a = title.find("a")
-        titletext = html.escape(a.text.strip())
-        if titletext in offers:
-            print('ollllld')
-            break
+        titletext = a.text.strip()
         href = a["href"]
+        offerURL = "http://www.wg-gesucht.de/en/" + href
+        if offerURL in newoffers:
+            break
         offer = getSoup(
             "http://www.wg-gesucht.de/en/" + href)
         english = offer.find("img", class_="flgS f-en")
         if not english:
             continue
         else:
-            validoffer = {}
-            validoffer["desc"] = html.escape(offer.find(
-                "div", class_="freitext wordWrap").text.strip().replace('\n', ""))
-            validoffer["price"] = html.escape(offer.find_all(
-                "div", class_="col-xs-6 text-center print_inline")[1].find("h2").text.strip())
-            validoffer["size"] = html.escape(offer.find_all(
-                "div", class_="col-xs-6 text-center print_inline")[0].find("h2").text.strip())
-            validoffer["href"] = html.escape(
-                "http://www.wg-gesucht.de/en/" + href)
-            offers[titletext] = validoffer
-            print(counter)
-            print(validoffer)
-            savescores(offers, 'offers')
+            try:
+                validoffer = {}
+                validoffer["desc"] = offer.find(
+                    "div", class_="freitext wordWrap").text.strip().replace('\n', "").replace('\r', '')
+                validoffer["price"] = offer.find_all(
+                    "div", class_="col-xs-6 text-center print_inline")[1].find("h2").text.strip()
+                validoffer["size"] = offer.find_all(
+                    "div", class_="col-xs-6 text-center print_inline")[0].find("h2").text.strip()
+                validoffer["Title"] = titletext
+                newoffers[offerURL] = validoffer
+                print(titletext)
+            except:
+                continue
+    print(len(newoffers))
+    savescoresUnicode(newoffers, 'offers')
 
 
 while True:
     find_new_offers()
-    print('sleeping for 30 mins')
-    time.sleep(60 * 30)
+    print('sleeping for 15 mins')
+    time.sleep(60 * 15)
