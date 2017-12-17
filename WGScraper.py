37245f21ac2cc3292b4995c7ec68fab9b5ef6660
Mohamed Oun <mohamed3on@gmail.com>
2017-12-17 14:01:49 +0100

    Add function to exclude short term rentals

--- WGScraper.py
+++ WGScraper.py
@@ -1,19 +1,39 @@
 import time
+
 from commonfunctions import getSoup, savescoresUnicode
 
-newoffers = {}
+NEW_OFFERS = {}
+
+EARLIEST_MOVE_OUT_MONTH = 6
+EARLIEST_MOVE_OUT_YEAR = 18
+
+
+def is_long_term(ad):
+    try:
+        move_out = ad.find(
+            "p", attrs={'style': 'line-height: 1.2em;'}).text.strip().split('-')[-1].split('.')
+        if int(move_out[1]) < EARLIEST_MOVE_OUT_MONTH or int(move_out[2]) < EARLIEST_MOVE_OUT_YEAR:
+            return False
+        else:
+            return True
+    except:
+        return True
 
 
 def find_new_offers():
-    soup = getSoup('http://www.wg-gesucht.de/en/wg-zimmer-in-Berlin.8.0.1.0.html?offer_filter=1&noDeact=1&city_id=8&category=0&rent_type=0&rMax=800&dFr=1512082800&dTo=1513724400&radLat=52.5306438&radLng=13.38306829999999&radAdd=Mitte%2C+Berlin%2C+Germany&radDis=8000&wgSea=2&wgAge=22&wgMnF=1&fur=1')
+    soup = getSoup('http://www.wg-gesucht.de/en/wg-zimmer-in-Berlin.8.0.1.0.html?offer_filter=1&noDeact=1&city_id=8&category=0&rent_type=0&sMin=13&rMax=800&dFr=1513810800&dTo=1525125600&radLat=52.5306438&radLng=13.38306829999999&radAdd=Mitte%2C+Berlin%2C+Germany&radDis=7000&wgSea=2&wgAge=22&wgMnF=1&fur=1')
     counter = 0
-    for title in soup.find_all("h3", class_="headline headline-list-view noprint truncate_title"):
+    for ad in soup.find_all('div', class_="col-sm-8")[4:]:
+        if not is_long_term(ad):
+            continue
+        title = ad.find(
+            "h3", class_="headline headline-list-view noprint truncate_title")
         counter = counter + 1
         a = title.find("a")
         titletext = a.text.strip()
         href = a["href"]
-        offerURL = "http://www.wg-gesucht.de/en/" + href
-        if offerURL in newoffers:
+        offer_url = "http://www.wg-gesucht.de/en/" + href
+        if offer_url in NEW_OFFERS:
             break
         offer = getSoup(
             "http://www.wg-gesucht.de/en/" + href)
@@ -30,12 +50,12 @@ def find_new_offers():
                 validoffer["size"] = offer.find_all(
                     "div", class_="col-xs-6 text-center print_inline")[0].find("h2").text.strip()
                 validoffer["Title"] = titletext
-                newoffers[offerURL] = validoffer
+                NEW_OFFERS[offer_url] = validoffer
                 print(titletext)
             except:
                 continue
-    print(len(newoffers))
-    savescoresUnicode(newoffers, 'offers')
+    print(len(NEW_OFFERS))
+    savescoresUnicode(NEW_OFFERS, 'offers')
 
 
 while True:
