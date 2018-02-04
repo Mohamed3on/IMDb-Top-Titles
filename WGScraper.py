c0d582ca0b860c98465e52978fdcfa52be091fa1
Mohamed Oun <mohamed3on@gmail.com>
2018-02-04 22:59:47 +0100

    Open links in browser instead of saving them to file, remove is_long_term

--- WGScraper.py
+++ WGScraper.py
@@ -1,31 +1,14 @@
-import time
+from time import sleep
+import webbrowser
+from commonfunctions import getSoup
 
-from commonfunctions import getSoup, savescoresUnicode
-
-NEW_OFFERS = {}
-
-EARLIEST_MOVE_OUT_MONTH = 6
-EARLIEST_MOVE_OUT_YEAR = 18
-
-
-def is_long_term(ad):
-    try:
-        move_out = ad.find(
-            "p", attrs={'style': 'line-height: 1.2em;'}).text.strip().split('-')[-1].split('.')
-        if move_out[0].strip().isdigit():
-            if int(move_out[1]) < EARLIEST_MOVE_OUT_MONTH or int(move_out[2]) < EARLIEST_MOVE_OUT_YEAR:
-                return False
-        return True
-    except:
-        return True
+NEW_OFFERS = set()
 
 
 def find_new_offers(url, isFlat=False):
     soup = getSoup(url)
     counter = 0
     for ad in soup.find_all('div', class_="col-sm-8")[4:]:
-        if not is_long_term(ad):
-            continue
         title = ad.find(
             "h3", class_="headline headline-list-view noprint truncate_title")
         counter = counter + 1
@@ -35,32 +18,24 @@ def find_new_offers(url, isFlat=False):
         offer_url = "http://www.wg-gesucht.de/en/" + href
         if offer_url in NEW_OFFERS:
             break
-        offer = getSoup(
-            "http://www.wg-gesucht.de/en/" + href)
+        offer = getSoup(offer_url)
         if not isFlat:
             english = offer.find("img", class_="flgS f-en")
             if not english:
                 continue
-            else:
-                try:
-                    validoffer = {}
-                    validoffer["desc"] = offer.find(
-                        "div", class_="freitext wordWrap").text.strip().replace('\n', "").replace('\r', '')
-                    validoffer["price"] = offer.find_all(
-                        "div", class_="col-xs-6 text-center print_inline")[1].find("h2").text.strip()
-                    validoffer["size"] = offer.find_all(
-                        "div", class_="col-xs-6 text-center print_inline")[0].find("h2").text.strip()
-                    validoffer["Title"] = titletext
-                    NEW_OFFERS[offer_url] = validoffer
-                    print(titletext)
-                except:
-                    continue
-    savescoresUnicode(NEW_OFFERS, 'offers')
+        try:
+            webbrowser.open(offer_url, new=2)
+            NEW_OFFERS.add(offer_url)
+            print(titletext)
+        except:
+            continue
 
 
 while True:
-    find_new_offers('http://www.wg-gesucht.de/en/wg-zimmer-in-Berlin.8.0.1.0.html?offer_filter=1&noDeact=1&city_id=8&category=0&rent_type=0&sMin=13&rMax=800&dFr=1513810800&dTo=1525125600&radLat=52.5306438&radLng=13.38306829999999&radAdd=Mitte%2C+Berlin%2C+Germany&radDis=7000&wgSea=2&wgAge=22&wgMnF=1&fur=1')
-    # find_new_offers('https://www.wg-gesucht.de/en/1-zimmer-wohnungen-in-Berlin.8.1.1.0.html?offer_filter=1&stadt_key=8&sort_column=&sort_order=&noDeact=1&autocompinp=Berlin&country_code=&countrymanuel=&city_name=&city_id=8&category=1&rent_type=0&sMin=13&rMax=800&dFr=1513810800&hidden_dFrDe=&dTo=1525125600&hidden_dToDe=&radLat=52.5306438&radLng=13.38306829999999&radAdd=Mitte%2C+Berlin%2C+Germany&radDis=7000&hidden_wgFla=0&hidden_wgSea=2&hidden_wgSmo=0&hidden_wgAge=22&hidden_wgMnF=1&hidden_wgMxT=0&sin=0&exc=0&hidden_rmMin=0&hidden_rmMax=0&pet=0&fur=1', True)
-    print(len(NEW_OFFERS))
+    print("Rooms:")
+    find_new_offers('https://www.wg-gesucht.de/en/wg-zimmer-in-Berlin.8.0.1.0.html?offer_filter=1&noDeact=1&city_id=8&category=0&rent_type=2&sMin=18&rMax=600&dFr=1517785200&dTo=1525125600&radLat=52.5130863&radLng=13.417266899999959&radAdd=Br%C3%BCckenstra%C3%9Fe+10%2C+10179%2C+Berlin%2C+Germany&radDis=5000&wgSea=2&wgAge=22&wgMnF=1')
+    # print("Flats:")
+    # find_new_offers('https://www.wg-gesucht.de/en/1-zimmer-wohnungen-in-Berlin.8.1.1.0.html?offer_filter=1&sort_column=0&noDeact=1&city_id=8&category=1&rent_type=0&sMin=30&rMax=800&dFr=1513810800&dTo=1527804000&radLat=52.5127637&radLng=13.415594000000056&radAdd=Am+K%C3%B6llnischen+Park%2C+Berlin%2C+Germany&radDis=5000', True)
+    # print(len(NEW_OFFERS))
     print('sleeping for 15 mins')
-    time.sleep(60 * 15)
+    sleep(60 * 15)
